b2e629820564fa34041d35b37ddcb122202d1553
Hanieh farajzadeh <76624015+hanie7@users.noreply.github.com>
2021-01-28 14:19:16 +0330

    Update time.cpp

--- time.cpp
+++ time.cpp
@@ -6,52 +6,52 @@ using namespace std;
 
 class Time{
 	
-	private :
-		int hours, minutes, seconds;
+private :
+int hours, minutes, seconds;
 		
-	public:
-		Time(){
-			hours = 0;
-			minutes = 0;
-			seconds = 0;
-		}
+public:
+Time(){
+hours = 0;
+minutes = 0;
+seconds = 0;
+}
 		
-		Time(int h, int m, int s){
-			hours 	= max(0, min(h, 23)); 
-			minutes = max(0, min(m, 59));
-			seconds = max(0, min(s, 59));
-		}
+Time(int h, int m, int s){
+hours 	= max(0, min(h, 23)); 
+minutes = max(0, min(m, 59));
+seconds = max(0, min(s, 59));
+}
 		
-		int getHours(){
-			return hours;
-		}
+int getHours(){
+return hours;
+}
 		
-		int getMin(){
-			return minutes;
-		}
+int getMin(){
+return minutes;
+}
 		
-		int getSec(){
-			return seconds;
-		}
+int getSec(){
+return seconds;
+}
 		
-		void Display(){
-			cout  << setfill('0') << setw(2) <<  getHours() << ":" << setw(2) <<  getMin() << ":" << setw(2) << getSec() << "\n";
-		}
+void Display(){
+cout  << setfill('0') << setw(2) <<  getHours() << ":" << setw(2) <<  getMin() << ":" << setw(2) << getSec() << "\n";
+}
 		
-		void add(Time x)
-		{
-			seconds = getSec() + x.getSec();
-			minutes = getMin() + x.getMin();
-			hours = getHours() + x.getHours();
+void add(Time x)
+{
+seconds = getSec() + x.getSec();
+minutes = getMin() + x.getMin();
+hours = getHours() + x.getHours();
 			
-			if(seconds >= 60)
-				minutes ++;
-			if(minutes >= 60)
-				hours ++;
+if(seconds >= 60)
+minutes ++;
+if(minutes >= 60)
+hours ++;
 			
-			seconds %= 60;
-			minutes %= 60;
-			hours %= 24;
+seconds %= 60;
+minutes %= 60;
+hours %= 24;
 		}
 		
 };
